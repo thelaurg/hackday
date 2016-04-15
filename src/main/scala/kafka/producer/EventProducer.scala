bab28e6fe2fe5b6efbdab5ffd0c1e014fa2ac8e1
rvijayan <raveen.vijayan@pure360.com>
2016-04-15 12:48:46 +0100

    event-producer

---
+++ src/main/scala/kafka/producer/EventProducer.scala
@@ -0,0 +1,30 @@
+package kafka.producer
+
+import kafka.producer.ProducerConfig
+import java.util.Properties
+import kafka.producer.Producer
+import scala.util.Random
+import kafka.producer.KeyedMessage
+
+case class EventProducer(eventType: String, topic: String, brokerList: String) {
+
+  def send(random: => String) = {
+    val props =  new Properties()
+
+    props.put("metadata.broker.list", brokerList)
+    props.put("serializer.class", "kafka.serializer.StringEncoder")
+    props.put("producer.type", "async")
+
+    val config = new ProducerConfig(props)
+
+    val producer = new Producer[String, String](config)
+
+    (1 to 10000).foreach { _ =>
+      val data = new KeyedMessage[String, String](topic, eventType, random)
+
+      producer.send(data)
+    }
+
+    producer.close()
+  }
+}
